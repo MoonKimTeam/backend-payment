3abe0f9ac855dd906f4372266e00ae0972fb5197
김성현 <beyool95@naver.com>
2024-02-04 14:23:42 +0900

    feat: entity, docker-compose 추가 (#4)
    * chore: add docker-compose
    
    * feat: BaseEntity 구현
    
    * feat: payment entity 추가
    
    * feat: payment repository 추가
    
    * feat: payment entity column nullable 옵션 추가

---
+++ src/main/kotlin/com/payment/domain/payment/model/Payment.kt
@@ -0,0 +1,35 @@
+package com.payment.domain.payment.model
+
+import com.payment.global.entity.BaseEntity
+import jakarta.persistence.*
+import org.hibernate.annotations.Comment
+import java.math.BigDecimal
+
+@Entity
+@Table(name = "payment")
+class Payment(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long = 0L,
+
+    @Comment("예약 id")
+    @Column(name = "reservation_id", nullable = false)
+    var reservationId: Long,
+
+    @Comment("결제자 이메일")
+    @Column(name = "email", nullable = false)
+    var email: String,
+
+    @Comment("결제자 휴대폰 번호")
+    @Column(name = "phone_number", nullable = false)
+    var phoneNumber: String,
+
+    @Comment("결제자 이름")
+    @Column(name = "name", nullable = false)
+    var name: String,
+
+    @Comment("결제 가격")
+    @Column(name = "price", nullable = false)
+    var price: BigDecimal
+): BaseEntity()
